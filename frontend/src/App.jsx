c096b8ce84c21ff887055b3b935a339afcce3555
jayanth0075 <jayanth9b.vhs@gmail.com>
2026-05-15 22:29:38 +0530

    feat: Set up React app with routing and Google OAuth provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,53 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import { Navbar } from "./components/Navbar"
+import { PrivateRoute } from "./components/PrivateRoute"
 import { HomePage } from "./pages/HomePage"
 import { Dashboard } from "./pages/Dashboard"
 import { MyOrders } from "./pages/MyOrders"
 import { MyPayments } from "./pages/MyPayments"
+import { LoginPage } from "./pages/LoginPage"
+import { RegisterPage } from "./pages/RegisterPage"
 import './App.css'
-function App() {
 
+const GOOGLE_CLIENT_ID = '216160325704-p90c0v8fgmd4i3605mt8d3mavlk9ttv9.apps.googleusercontent.com'
+
+function App() {
   return (
-    <>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/myorders" element={<MyOrders />} />
-          <Route path="/mypayments" element={<MyPayments />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/myorders"
+            element={
+              <PrivateRoute>
+                <MyOrders />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/mypayments"
+            element={
+              <PrivateRoute>
+                <MyPayments />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </Router>
-    </>
+    </GoogleOAuthProvider>
   )
 }
 
